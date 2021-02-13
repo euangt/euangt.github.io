1ab62ec67f320bd8f635098ef4a022a55372ea43
euangt <euan.gillespie@outlook.com>
2021-02-13 11:31:50 +0000

    failed scroll implementation

---
+++ application/javascript/init_scroll.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  $('.project-slider').slick({
+    scenterMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
